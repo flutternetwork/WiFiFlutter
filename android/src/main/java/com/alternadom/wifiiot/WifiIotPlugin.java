18d8905d7ec53e6cfa5a806192d508e592190a8c
Harsh Bhikadia <harsh.bhikadiya@gmail.com>
2020-10-29 17:38:05 +0530

    [android] removed `sudoForResult`, `readFully`, `Closer`

--- android/src/main/java/com/alternadom/wifiiot/WifiIotPlugin.java
+++ android/src/main/java/com/alternadom/wifiiot/WifiIotPlugin.java
@@ -23,13 +23,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.ByteArrayOutputStream;
-import java.io.Closeable;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.DatagramSocket;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -841,6 +834,8 @@ public class WifiIotPlugin implements FlutterPlugin, ActivityAware, MethodCallHa
                     poResult.success(false);
                 }
             }, handler, 30 * 1000);
+
+            // TODO remove network in cleanup, if joinOnce
         }
     }
 
@@ -935,70 +930,6 @@ public class WifiIotPlugin implements FlutterPlugin, ActivityAware, MethodCallHa
         }
 
         return connected;
-    }
-
-    public static String sudoForResult(String... strings) {
-        String res = "";
-        DataOutputStream outputStream = null;
-        InputStream response = null;
-        try {
-            Process su = Runtime.getRuntime().exec("su");
-            outputStream = new DataOutputStream(su.getOutputStream());
-            response = su.getInputStream();
-
-            for (String s : strings) {
-                outputStream.writeBytes(s + "\n");
-                outputStream.flush();
-            }
-
-            outputStream.writeBytes("exit\n");
-            outputStream.flush();
-            try {
-                su.waitFor();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            res = readFully(response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            Closer.closeSilently(outputStream, response);
-        }
-        return res;
-    }
-
-    private static String readFully(InputStream is) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int length = 0;
-        while ((length = is.read(buffer)) != -1) {
-            baos.write(buffer, 0, length);
-        }
-        return baos.toString("UTF-8");
-    }
-
-    static class Closer {
-        /// closeAll()
-        public static void closeSilently(Object... xs) {
-            /// Note: on Android API levels prior to 19 Socket does not implement Closeable
-            for (Object x : xs) {
-                if (x != null) {
-                    try {
-                        if (x instanceof Closeable) {
-                            ((Closeable) x).close();
-                        } else if (x instanceof Socket) {
-                            ((Socket) x).close();
-                        } else if (x instanceof DatagramSocket) {
-                            ((DatagramSocket) x).close();
-                        } else {
-                            throw new RuntimeException("cannot close " + x);
-                        }
-                    } catch (Throwable e) {
-                        // TODO : do something ?
-                    }
-                }
-            }
-        }
     }
 }
 
