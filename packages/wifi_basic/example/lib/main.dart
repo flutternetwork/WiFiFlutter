6a3408145edb9b87a027dfbfc40bc22fffea9566
Harsh Bhikadia <harsh.bhikadiya@gmail.com>
2021-11-28 19:46:11 +0530

    formatting

--- packages/wifi_basic/example/lib/main.dart
+++ packages/wifi_basic/example/lib/main.dart
@@ -17,62 +17,62 @@ class _MyAppState extends State<MyApp> {
   void showSnackbar(BuildContext context, String message) {
     ScaffoldMessenger.of(context).hideCurrentSnackBar();
     ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text(message)));
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Builder(
-        builder: (context) => Scaffold(
-            appBar: AppBar(
-              title: const Text('Plugin example app'),
-            ),
-            body: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
+          builder: (context) => Scaffold(
+                appBar: AppBar(
+                  title: const Text('Plugin example app'),
+                ),
+                body: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const Text("WiFiBasic.hasCapability:"),
-                      TextButton(
-                        child: const Text("call"),
-                        onPressed: () async => showSnackbar(context,
-                            "hasCapability: ${await WiFiBasic.hasCapability()}"),
+                      Row(
+                        children: [
+                          const Text("WiFiBasic.hasCapability:"),
+                          TextButton(
+                            child: const Text("call"),
+                            onPressed: () async => showSnackbar(context,
+                                "hasCapability: ${await WiFiBasic.hasCapability()}"),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Text("WiFiBasic.isEnabled:"),
+                          TextButton(
+                            child: const Text("call"),
+                            onPressed: () async => showSnackbar(context,
+                                "isEnabled: ${await WiFiBasic.isEnabled()}"),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Text("WiFiBasic.setEnabled:"),
+                          TextButton(
+                            child: const Text("enable"),
+                            onPressed: () async => showSnackbar(context,
+                                "setEnabled(true): ${await WiFiBasic.setEnabled(true, shouldOpenSettings: true)}"),
+                          ),
+                          TextButton(
+                            child: const Text("disable"),
+                            onPressed: () async => showSnackbar(context,
+                                "setEnabled(false): ${await WiFiBasic.setEnabled(false, shouldOpenSettings: true)}"),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                  Row(
-                    children: [
-                      const Text("WiFiBasic.isEnabled:"),
-                      TextButton(
-                        child: const Text("call"),
-                        onPressed: () async => showSnackbar(
-                            context, "isEnabled: ${await WiFiBasic.isEnabled()}"),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      const Text("WiFiBasic.setEnabled:"),
-                      TextButton(
-                        child: const Text("enable"),
-                        onPressed: () async => showSnackbar(context,
-                            "setEnabled(true): ${await WiFiBasic.setEnabled(true, shouldOpenSettings: true)}"),
-                      ),
-                      TextButton(
-                        child: const Text("disable"),
-                        onPressed: () async => showSnackbar(context,
-                            "setEnabled(false): ${await WiFiBasic.setEnabled(false, shouldOpenSettings: true)}"),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          )
-      ),
+                ),
+              )),
     );
   }
 }
