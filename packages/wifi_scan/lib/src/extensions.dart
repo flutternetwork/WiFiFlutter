eb94c6dfa78a876dff0245abea7b7394adbf281e
Harsh Bhikadia <harsh.bhikadiya@gmail.com>
2021-12-05 14:47:54 +0530

    formatting

--- packages/wifi_scan/lib/src/extensions.dart
+++ packages/wifi_scan/lib/src/extensions.dart
@@ -1,7 +1,7 @@
 import 'package:wifi_scan/wifi_scan.dart';
 
-extension ToEnumExtension on int{
-  toCanRequestScan()=> CanRequestScan.values[this];
+extension ToEnumExtension on int {
+  toCanRequestScan() => CanRequestScan.values[this];
 
   toCanGetScannedNetworks() => CanGetScannedNetworks.values[this];
 }
