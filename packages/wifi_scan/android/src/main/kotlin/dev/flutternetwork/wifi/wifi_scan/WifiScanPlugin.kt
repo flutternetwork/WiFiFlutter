086bdf4776e7e7637e2a936839ff3923daddedfd
Harsh Bhikadia <harsh.bhikadiya@gmail.com>
2021-12-12 12:08:00 +0530

    [android,ios] methods scaffolded

--- packages/wifi_scan/android/src/main/kotlin/dev/flutternetwork/wifi/wifi_scan/WifiScanPlugin.kt
+++ packages/wifi_scan/android/src/main/kotlin/dev/flutternetwork/wifi/wifi_scan/WifiScanPlugin.kt
@@ -1,5 +1,7 @@
 package dev.flutternetwork.wifi.wifi_scan
 
+import android.content.Context
+import android.net.wifi.WifiManager
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -8,28 +10,75 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+/** CanStartScan codes */
+private const val CAN_START_SCAN_NOT_SUPPORTED = 0
+private const val CAN_START_SCAN_YES = 1
+private const val CAN_START_SCAN_NO_LOCATION_PERMISSION_REQUIRED = 2
+private const val CAN_START_SCAN_NO_LOCATION_PERMISSION_DENIED = 3
+private const val CAN_START_SCAN_NO_LOCATION_SERVICE_DISABLED = 4
+
+/** CanGetScannedResults codes */
+private const val CAN_GET_SCANNED_RESULTS_NOT_SUPPORTED = 0
+private const val CAN_GET_SCANNED_RESULTS_YES = 1
+private const val CAN_GET_SCANNED_RESULTS_NO_LOCATION_PERMISSION_REQUIRED = 2
+private const val CAN_GET_SCANNED_RESULTS_LOCATION_PERMISSION_DENIED = 3
+private const val CAN_GET_SCANNED_RESULTS_LOCATION_SERVICE_DISABLED = 4
+
 /** WifiScanPlugin */
-class WifiScanPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class WifiScanPlugin : FlutterPlugin, MethodCallHandler {
+    private lateinit var channel: MethodChannel
+    private lateinit var context: Context
+    private var wifi: WifiManager? = null
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "wifi_scan")
-    channel.setMethodCallHandler(this)
-  }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "wifi_scan")
+        channel.setMethodCallHandler(this)
+        context = flutterPluginBinding.applicationContext
+        wifi =
+            context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        // TODO: handle wifi_scan/scannedNetworksEvent eventChannel
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+            "canStartScan" -> {
+                val askPermission = call.argument<Boolean>("askPermissions") ?: return result.error(
+                    "InvalidArg",
+                    "askPermissions argument is null",
+                    null
+                )
+                result.success(canStartScan(askPermission))
+            }
+            "startScan" -> result.success(startScan())
+            "canGetScannedNetworks" -> {
+                val askPermission = call.argument<Boolean>("askPermissions") ?: return result.error(
+                    "InvalidArg",
+                    "askPermissions argument is null",
+                    null
+                )
+                result.success(canGetScannedNetworks(askPermission))
+            }
+            "scannedNetworks" -> result.success(scannedNetworks())
+            else -> result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+        wifi = null
+    }
+
+    // TODO
+    private fun canStartScan(askPermission: Boolean): Int = CAN_START_SCAN_NOT_SUPPORTED
+
+    // TODO
+    private fun startScan(): Boolean = false
+
+    // TODO
+    private fun canGetScannedNetworks(askPermission: Boolean): Int =
+        CAN_GET_SCANNED_RESULTS_NOT_SUPPORTED
+
+    // TODO
+    private fun scannedNetworks(): List<Map<String, Any>> = emptyList()
 }
--- packages/wifi_scan/ios/Classes/SwiftWifiScanPlugin.swift
+++ packages/wifi_scan/ios/Classes/SwiftWifiScanPlugin.swift
@@ -6,9 +6,21 @@ public class SwiftWifiScanPlugin: NSObject, FlutterPlugin {
     let channel = FlutterMethodChannel(name: "wifi_scan", binaryMessenger: registrar.messenger())
     let instance = SwiftWifiScanPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    // TODO: handle wifi_scan/scannedNetworksEvent eventChannel
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch(call.method){
+      case "canStartScan":
+          return result(0) // not supported
+      case "startScan":
+          return result(false) // always fails
+      case "canGetScannedNetworks":
+          return result(0) // not supported
+      case "scannedNetworks":
+          return result([]) // empty results
+      default:
+          return result(FlutterMethodNotImplemented)
+    }
   }
 }
